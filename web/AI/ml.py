6cd7fc67361c8068e9e825dfc12876090a84362d
bener07 <bernandre07@gmail.com>
2022-12-17 20:50:13 +0000

    AI and ml done

--- web/AI/ml.py
+++ web/AI/ml.py
@@ -15,3 +15,268 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout
 
 # Loading in the Data
 df = pd.read_csv("SPY.csv")
+
+class Mokita:
+    def __init__(self, coin, exchange):
+        self.coin = coin
+        self.exc = exchange
+
+    ## Scaling
+    # Scale fitting the close prices separately for inverse_transformations purposes later
+    close_scaler = RobustScaler()
+
+    close_scaler.fit(df[['Close']])
+
+    # Normalizing/Scaling the DF
+    scaler = RobustScaler()
+
+    df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns, index=df.index)
+
+    def split_sequence(seq, n_steps_in, n_steps_out):
+        """
+        Splits the multivariate time sequence
+        """
+        # Creating a list for both variables
+        X, y = [], []
+        for i in range(len(seq)):
+
+            # Finding the end of the current sequence
+            end = i + n_steps_in
+            out_end = end + n_steps_out
+
+            # Breaking out of the loop if we have exceeded the dataset's length
+            if out_end > len(seq):
+                break
+
+            # Splitting the sequences into: x = past prices and indicators, y = prices ahead
+            seq_x, seq_y = seq[i:end, :], seq[end:out_end, 0]
+
+            X.append(seq_x)
+            y.append(seq_y)
+
+        return np.array(X), np.array(y)
+
+    def visualize_training_results(results):
+        """
+        Plots the loss and accuracy for the training and testing data
+        """
+        history = results.history
+        plt.figure(figsize=(16,5))
+        plt.plot(history['val_loss'])
+        plt.plot(history['loss'])
+        plt.legend(['val_loss', 'loss'])
+        plt.title('Loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.show()
+
+        plt.figure(figsize=(16,5))
+        plt.plot(history['val_accuracy'])
+        plt.plot(history['accuracy'])
+        plt.legend(['val_accuracy', 'accuracy'])
+        plt.title('Accuracy')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.show()
+
+        def layer_maker(n_layers, n_nodes, activation, drop=None, d_rate=.5):
+            """
+            Creates a specified number of hidden layers for an RNN
+            Optional: Adds regularization option - the dropout layer to prevent potential overfitting (if necessary)
+            """
+
+            # Creating the specified number of hidden layers with the specified number of nodes
+            for x in range(1,n_layers+1):
+                model.add(LSTM(n_nodes, activation=activation, return_sequences=True))
+
+                # Adds a Dropout layer after every Nth hidden layer (the 'drop' variable)
+                try:
+                    if x % drop == 0:
+                        model.add(Dropout(d_rate))
+                except:
+                    pass
+
+        def validater(self, n_per_in, n_per_out):
+            """
+            Runs a 'For' loop to iterate through the length of the DF and create predicted values for every stated interval
+            Returns a DF containing the predicted values for the model with the corresponding index values based on a business day frequency
+            """
+            # Creating an empty DF to store the predictions
+            predictions = pd.DataFrame(index=df.index, columns=[df.columns[0]])
+
+            for i in range(n_per_in, len(df)-n_per_in, n_per_out):
+                # Creating rolling intervals to predict off of
+                x = df[-i - n_per_in:-i]
+
+                # Predicting using rolling intervals
+                yhat = model.predict(np.array(x).reshape(1, n_per_in, self.n_features))
+
+                # Transforming values back to their normal prices
+                yhat = self.close_scaler.inverse_transform(yhat)[0]
+
+                # DF to store the values and append later, frequency uses business days
+                pred_df = pd.DataFrame(yhat,
+                                    index=pd.date_range(start=x.index[-1],
+                                                        periods=len(yhat),
+                                                        freq="B"),
+                                    columns=[x.columns[0]])
+
+                # Updating the predictions DF
+                predictions.update(pred_df)
+
+            return predictions
+
+        def val_rmse(df1, df2):
+            """
+            Calculates the root mean square error between the two Dataframes
+            """
+            df = df1.copy()
+
+            # Adding a new column with the closing prices from the second DF
+            df['close2'] = df2.Close
+
+            # Dropping the NaN values
+            df.dropna(inplace=True)
+
+            # Adding another column containing the difference between the two DFs' closing prices
+            df['diff'] = df.Close - df.close2
+
+            # Squaring the difference and getting the mean
+            rms = (df[['diff']]**2).mean()
+
+            # Returning the sqaure root of the root mean square
+            return float(np.sqrt(rms))
+
+    def predict(self):
+        if self.neuralnet is None:
+            self.createNn()
+        df = pd.read_csv(f'../../api/app/csvs/{self.coin}/info.csv')
+        ## Datetime conversion
+        df['Date'] = pd.to_datetime(df.Date)
+
+        # Setting the index
+        df.set_index('Date', inplace=True)
+
+        # Dropping any NaNs
+        df.dropna(inplace=True)
+
+
+        ## Technical Indicators
+
+        # Adding all the indicators
+        df = ta.add_all_ta_features(df, open="Open", high="High", low="Low", close="Close", volume="Volume", fillna=True)
+
+        # Dropping everything else besides 'Close' and the Indicators
+        df.drop(['Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1, inplace=True)
+
+        # Only using the last 1000 days of data to get a more accurate representation of the current market climate
+        df = df.tail(1000)
+
+        # Predicting off of the most recent days from the original DF
+        yhat = model.predict(np.array(df.tail(self.n_per_in)).reshape(1, self.n_per_in, self.n_features))
+
+        # Transforming the predicted values back to their original format
+        yhat = self.close_scaler.inverse_transform(yhat)[0]
+
+        # Creating a DF of the predicted prices
+        preds = pd.DataFrame(yhat,
+                            index=pd.date_range(start=df.index[-1]+timedelta(days=1),
+                                                periods=len(yhat),
+                                                freq="B"),
+                            columns=[df.columns[0]])
+
+        # Number of periods back to plot the actual values
+        pers = self.n_per_in
+
+        # Transforming the actual values to their original price
+        actual = pd.DataFrame(self.close_scaler.inverse_transform(df[["Close"]].tail(pers)),
+                            index=df.Close.tail(pers).index,
+                            columns=[df.columns[0]]).append(preds.head(1))
+
+        # Printing the predicted prices
+
+        # Plotting
+        plt.figure(figsize=(16,6))
+        plt.plot(actual, label="Actual Prices")
+        plt.plot(preds, label="Predicted Prices")
+        plt.ylabel("Price")
+        plt.xlabel("Dates")
+        plt.title(f"Forecasting the next {len(yhat)} days")
+        plt.legend()
+        plt.show()
+
+    def createNn(self):
+        ## Creating the NN
+        # How many periods looking back to learn
+        n_per_in  = 120
+        # How many periods to predict
+        n_per_out = 50
+        # Features
+        n_features = df.shape[1]
+        # Splitting the data into appropriate sequences
+        X, y = self.split_sequence(df.to_numpy(), n_per_in, n_per_out)
+
+        # Instatiating the model
+        model = Sequential()
+
+        # Activation
+        activ = "tanh"
+
+        # Input layer
+        model.add(LSTM(90,
+                    activation=activ,
+                    return_sequences=True,
+                    input_shape=(n_per_in, n_features)))
+
+        # Hidden layers
+        self.layer_maker(n_layers=1,
+                    n_nodes=35,
+                    activation=activ)
+
+        # Final Hidden layer
+        model.add(LSTM(60, activation=activ))
+
+        # Output layer
+        model.add(Dense(n_per_out))
+
+        # Model summary
+        model.summary()
+
+        # Compiling the data with selected specifications
+        model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+
+        ## Fitting and Training
+        res = model.fit(X, y, epochs=90, batch_size=128, validation_split=0.1)## Creating the NN
+
+        # Instatiating the model
+        model = Sequential()
+
+        # Activation
+        activ = "tanh"
+
+        # Input layer
+        model.add(LSTM(90,
+                    activation=activ,
+                    return_sequences=True,
+                    input_shape=(n_per_in, n_features)))
+
+        # Hidden layers
+        self.layer_maker(n_layers=1,
+                    n_nodes=30,
+                    activation=activ)
+
+        # Final Hidden layer
+        model.add(LSTM(60, activation=activ))
+
+        # Output layer
+        model.add(Dense(n_per_out))
+
+        # Model summary
+        model.summary()
+
+        # Compiling the data with selected specifications
+        model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+
+        ## Fitting and Training
+        res = model.fit(X, y, epochs=120, batch_size=128, validation_split=0.1)
+        self.neuralnet = res

175dd3621887ef7f76c5cd632d46823f55e3b3f2
bener07 <bernandre07@gmail.com>
2022-12-18 12:41:34 +0000

    T

--- api/app/csvs/__init__.py
+++
@@ -1,6 +0,0 @@
-def getCsvsData(name):
-    import csv
-    with open(f'csvs/{name.upper()}/info.csv') as file:
-        reader = csv.reader(file)
-        data = [[i[0],i[1]] for i in reader]
-        return data

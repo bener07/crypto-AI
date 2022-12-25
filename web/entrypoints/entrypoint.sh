4dab080de2be7c319bc56f56c12a368d843b656c
bener07 <bernandre07@gmail.com>
2022-12-25 21:12:33 +0000

    Problems with docker-compose and entrypoints

--- web/entrypoints/entrypoint.sh
+++ web/entrypoints/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if [ $SERVER_PORT ]
+if [ $SERVER_HOST_PORT ]
 then
-    echo "Using port: $SERVER_PORT\n";
+    echo "Using port: $SERVER_HOST_PORT\n";
 else
     echo "Using default port 8000";
-    SERVER_PORT = 8000;
+    SERVER_HOST_PORT = 8000;
 fi
-echo $SERVER_PORT
-python3 manage.py runserver $SERVER_PORT
+echo $SERVER_HOST_PORT
+python3 manage.py runserver $SERVER_HOST_PORT

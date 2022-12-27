9c0f4c2bdb19288712cd7c49312c3e72029780e7
bener07 <bernandre07@gmail.com>
2022-12-27 19:33:20 +0000

    Csv and other stuff

---
+++ web/Docker/csv_shell.sh
@@ -0,0 +1,63 @@
+#!/home/bernandre07/.conda/envs/env/bin/python3
+import os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crypto_wallet.settings')
+django.setup()
+from csv_updater import run_csvs
+from database.models import coins
+import sys
+
+
+config = {
+    'command_pallete': ">>>",
+    'loggingLevel': 'warning',
+}
+
+def csvs():
+    try:
+        logging.basicConfig(level=config['loggingLevel'])
+    except:
+        print("Check your configurations over loggingLevel")
+    finally:
+        run_csvs()
+
+
+def panel():
+    print("""
+ _____________________________________________________
+|     ____        __           _____ __         ____  |
+|    / __ \____ _/ /_____ _   / ___// /_  ___  / / /  |
+|   / / / / __ `/ __/ __ `/   \__ \/ __ \/ _ \/ / /   |
+|  / /_/ / /_/ / /_/ /_/ /   ___/ / / / /  __/ / /    |
+| /_____/\__,_/\__/\__,_/   /____/_/ /_/\___/_/_/     |
+|_____________________________________________________|
+        """)
+
+def command_pallete(chars):
+    execute(*input(f"{chars} ").split(' '))
+
+def execute(*args, **kwargs):
+    if args[0] == 'shell':
+        return os.system(" ".join(args[1:]))
+    if args[0] == 'clear':
+        return os.system('clear')
+    if args[0] == 'set':
+        config[args[1]] = " ".join(args[2:])
+        return 0
+    if args[0] == 'show':
+        if len(args) <= 1:
+            return print("You need to specify more arguments.\nEX: show config")
+        if args[1] == 'config':
+            return print(config)
+    if args[0] == 'run':
+        if args[1] == 'updater':
+            csvs()
+    else:
+        print("Command not found!")
+if __name__ == "__main__":
+    panel()
+    while True:
+        try:
+            command_pallete(config.get('command_pallete'))
+        except KeyboardInterrupt:
+            print("\nByeee!")
+            break
--- web/Docker/csv_updater.py
+++ web/Docker/csv_updater.py
@@ -7,6 +7,7 @@ django.setup()
 from database.models import coins
 import requests, threading, time
 import pandas as pd
+import logging
 
 def update(*args, limit=10):
     sym, exc, action = args
@@ -20,45 +21,56 @@ def update(*args, limit=10):
     return response
 
 
+class thread(threading.Thread):
+    def __init__(self, name, loggingLevel):
+        self.loggingLevel = loggingLevel
+        self.k = False
+        self.name = name
+        self.format = '\033[0;31m%(levelname)s\033[0m:\t %(message)s'
 
 
-def thread(n):
-    import time
-    cicle=0
-    print(f'Coin {n.coin_name :5}:   running!')
-    try:
-        os.mkdir(f'../../api/app/csvs/{n.coin_name}')
-        open(f'../../api/app/csvs/{n.coin_name}/info.csv', 'w').write('Date, Price\n')
-    except FileExistsError:
-        pass
-    finally:
-      while True:
+class csvs:
+    def kill(self):
+        self.k = True
+    def thread(self, n):
+        if self.loggingLevel == 'debug':
+            logging.basicConfig(level=logging.DEBUG, format=self.format)
+        if self.loggingLevel == 'info':
+            logging.basicConfig(level=logging.INFO, format=self.format)
+        elif self.loggingLevel == 'warning':
+            logging.basicConfig(level=logging.WARNING, format=self.format)
+        import time
+        cicle=0
+        logging.info(f'Coin {n.coin_name :5}:   running!')
+        self.k = False
         try:
-            cicle += 1
-            request = update(n.coin_name, 'eur', 'price')
-            date = request.get('time')
-            price = request.get('price')
-            data = [
-            [date, price]
-            ]
-            coins.objects.filter(coin_name=n.coin_name).update(price=price)
-            df = pd.DataFrame(data)
-            df.to_csv(f'../../api/app/csvs/{n.coin_name}/info.csv', index=False, mode='a', header=False)
-            print(f'Coin {n.coin_name:5} in cicle {cicle:3}    status:   updated!')
-        except:
-            print(f'Coin {n.coin_name:5} in cicle {cicle:3}    status:   disconnected!\a\7')
-            continue
+            os.mkdir(f'../../api/app/csvs/{n.coin_name}')
+            logging.warning(f'../../api/app/csvs/{n.coin_name}/info.csv', 'w').write('Date, Price\n')
+        except FileExistsError:
+            pass
         finally:
-            time.sleep(60)
-
-
-def run_csvs():
-    for coin in coins.current_coins():
-        th = threading.Thread(target=thread, args=(coin,))
-        time.sleep(0.2)
-        th.start()
-
-
-
-if __name__ == "__main__":
-    run_csvs()
+            while not self.k:
+                try:
+                    cicle += 1
+                    request = update(n.coin_name, 'eur', 'price')
+                    date = request.get('time')
+                    price = request.get('price')
+                    # writing the data inside a list
+                    data = [
+                    [date, price]
+                    ]
+                    coins.objects.filter(coin_name=n.coin_name).update(price=price) #update the price of the coing inside the Database
+                    df = pd.DataFrame(data) # create a dataframe
+                    df.to_csv(f'../../api/app/csvs/{n.coin_name}/info.csv', index=False, mode='a', header=False) # write the Dataframe inside a CSV
+                    logging.info(f'Coin {n.coin_name:5} in cicle {cicle:3}    status:   updated!')
+                except:
+                    logging.warning(f'Coin {n.coin_name:5} in cicle {cicle:3}    status:   disconnected!\a\7')
+                    continue
+                finally:
+                    time.sleep(60)
+            logging.info(f'Coin {n.coin_name:5} in cicle {cicle:3}    status:   Killed!')
+    def run_csvs(self):
+        for coin in coins.current_coins():
+            th = threading.Thread(target=self.thread, args=(coin,))
+            time.sleep(0.1)
+            th.start()

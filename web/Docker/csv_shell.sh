013657cb248986c15c6330c4f5389ecca095f23e
bener07 <bernandre07@gmail.com>
2022-12-30 20:26:11 +0000

    Manager done

--- web/Docker/csv_shell.sh
+++ web/Docker/csv_shell.sh
@@ -1,20 +1,35 @@
 #!/home/bernandre07/.conda/envs/env/bin/python3
-import os, django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crypto_wallet.settings')
-django.setup()
-from csv_updater import run_csvs
+from csv_updater import thread
 from database.models import coins
-import sys
+import os
+from threading import Thread
+import json
 
 
 config = {
     'command_pallete': ">>>",
     'loggingLevel': 'info',
+    'projectBaseDir': '/home/bernandre07/Desktop/dev/github-projects/AI/crypto-Ai/',
+    'djangoDir': '',
+    'scriptPath': os.getcwd(),
+    'projectServices': {
+        'webserver': 'web',
+        'api': 'api',
+    },
+    'projectNetworks':{
+        'frontend': 'frontendnet',
+        'backend': 'backendnet',
+    },
 }
 
 
-
-def panel():
+def begining():
+    try:
+        with open("dataShellConfig.json", "r") as file:
+            global config
+            config = json.load(file)
+    except FileNotFoundError:
+        print("Using default config")
     print("""
  _____________________________________________________
 |     ____        __           _____ __         ____  |
@@ -28,29 +43,84 @@ def panel():
 def command_pallete(chars):
     execute(*input(f"{chars} ").split(' '))
 
+def environment_setup():
+    os.chdir(config['projectBaseDir'])
+    if config['djangoDir'] == '':
+        print("\033[0;31mWarning!\033[0m Please specify the django path or the script won't work properly")
+
+def byee():
+    print("\nByeee!")
+    with open('dataShellConfig.json', 'w') as file:
+        file.write(json.dumps(config))
+    exit()
+
+
+
+def manage_api(command):
+    if command == 'up':
+        os.system(f"docker-compose up -d api")
+    if command == 'logs':
+        os.system(f"docker-compose logs --tail=10 -f api")
+    if command == 'stop' or command == 'down' or command == 'kill':
+        os.system(f"docker-compose down")
+
+
+def manage_web(command):
+    if command == 'up':
+        os.system(f"docker-compose up -d web")
+    if command == 'logs':
+        os.system(f"docker-compose logs --tail=10 -f web")
+    if command == 'stop' or command == 'down' or command == 'kill':
+        os.system(f"docker-compose down")
+
+
+def csv_updater(coin):
+    global csv_thread_command
+    th = thread(coin, config['loggingLevel'])
+    if csv_thread_command == 'start' or csv_thread_command == 'run':
+        th.start()
+    if csv_thread_command == 'kill' or csv_thread_command == 'stop' and th.status != 'Killed':
+        th.kill()
+    if csv_thread_command == 'status':
+        print(th.config['status'])
+
+def coin_main_thread():
+    for coin in coins.current_coins():
+        csv_updater(coin)
+
 def execute(*args, **kwargs):
     if args[0] == 'shell':
         return os.system(" ".join(args[1:]))
-    if args[0] == 'clear':
+    elif args[0] == 'clear':
         return os.system('clear')
-    if args[0] == 'set':
+    elif args[0] == 'set':
         config[args[1]] = " ".join(args[2:])
         return 0
-    if args[0] == 'show':
+    elif args[0] == 'show':
         if len(args) <= 1:
             return print("You need to specify more arguments.\nEX: show config")
         if args[1] == 'config':
-            return print(config)
-    # if args[0] == 'run':
-    #     if args[1] == 'updater':
-    #         csvs(
+            return print(json.dumps(config, indent=2))
+    elif args[0] == 'updater':
+        global csv_thread_command
+        csv_thread_command = args[1]
+        th = Thread(target=coin_main_thread)
+        th.start()
+    elif args[0] == 'api':
+        manage_api(args[1])
+    elif args[0] == 'web':
+        manage_web(args[1])
+    elif args[0] == 'services':
+        print(config['projectServices'])
+    elif args[0] == 'quit' or args[0] == 'exit':
+        byee()
     else:
         print("Command not found!")
 if __name__ == "__main__":
-    panel()
+    begining()
     while True:
         try:
             command_pallete(config.get('command_pallete'))
         except KeyboardInterrupt:
-            print("\nByeee!")
+            byee()
             break
--- web/Docker/csv_updater.py
+++ web/Docker/csv_updater.py
@@ -11,6 +11,8 @@ import logging
 import time
 from threading import Event
 
+API_PATH = '../../api/app/csvs/'
+
 def update(*args, limit=10):
     sym, exc, action = args
     url = f"http://127.0.0.1:5000/{action}"
@@ -22,6 +24,7 @@ def update(*args, limit=10):
     response = requests.post(url, data=data).json()
     return response
 
+
 class thread(threading.Thread, threading.Event):
     def __init__(self, coin, loggingLevel):
         threading.Thread.__init__(self)
@@ -29,49 +32,66 @@ class thread(threading.Thread, threading.Event):
         self.event = Event()
         self.name = coin.coin_name # stores the name of the coin
         self.coin = coin # stores the coin object from the database
-        self.format = '\033[0;31m%(levelname)s\033[0m:\t %(message)s' # Allows to configure the format of the logging message
+        self.config = {
+            'format': '\033[0;31m%(levelname)s\033[0m:\t %(message)s',
+            'status': 'Not initiated',
+            'kill': False,
+        }
+
+    def kill(self):
+        global killed
+        killed = True
+        return 0
+
+    def sleep(self, secs):
+        # self.config['kill'] = False
+        counter = 0
+        global killed
+        while counter < secs:
+            if killed:
+                break
+            else:
+                counter += 1
+                time.sleep(1)
+        return 0
+
     def run(self):
         if self.loggingLevel == 'debug':
-            logging.basicConfig(level=logging.DEBUG, format=self.format)
+            logging.basicConfig(level=logging.DEBUG, format=self.config['format'])
         if self.loggingLevel == 'info':
-            logging.basicConfig(level=logging.INFO, format=self.format)
+            logging.basicConfig(level=logging.INFO, format=self.config['format'])
         elif self.loggingLevel == 'warning':
-            logging.basicConfig(level=logging.WARNING, format=self.format)
+            logging.basicConfig(level=logging.WARNING, format=self.config['format'])
         cicle=0
+        global killed
+        killed = False
         logging.info(f'Coin {self.name :5}:   running!') # logging message with the starting message status
         try:
-            os.mkdir(f'../../api/app/csvs/{self.name}')
-            logging.warning(f'../../api/app/csvs/{self.name}/info.csv', 'w').write('Date, Price\n')
+            os.mkdir(API_PATH+f'{self.name}')
+            open(API_PATH+f'{self.name}/info.csv', 'w').write('Date, Price\n')
         except FileExistsError:
             pass
         finally:
-            while not self.event.is_set():
+            while not killed:
                 try:
                     cicle += 1
                     request = update(self.name, 'eur', 'price')
                     date = request.get('time')
                     price = request.get('price')
+                    self.config['status'] = "Running"
                     # writing the data inside a list
                     data = [
                     [date, price]
                     ]
                     coins.objects.filter(coin_name=self.name).update(price=price) #update the price of the coing inside the Database
                     df = pd.DataFrame(data) # create a dataframe
-                    df.to_csv(f'../../api/app/csvs/{self.name}/info.csv', index=False, mode='a', header=False) # write the Dataframe inside a CSV
+                    df.to_csv(API_PATH+f'{self.name}/info.csv', index=False, mode='a', header=False) # write the Dataframe inside a CSV
                     logging.info(f'Coin {self.name:5} in cicle {cicle:3}    status:   updated!')
                 except:
                     logging.warning(f'Coin {self.name:5} in cicle {cicle:3}    status:   disconnected!\a\7')
+                    self.config['status'] = "Disconnected, check the api or Internet connection"
                     continue
                 finally:
-                    time.sleep(60)
-            logging.info(f'Coin {self.name:5} in cicle {cicle:3}    status:   Killed!')
-    def kill(self):
-        self.event.set()
-        sys.exit(0)
-
-
-def run_csvs(self):
-    for coin in coins.current_coins():
-        th = thread(coin)
-        time.sleep(0.1)
-        th.start()
+                        self.sleep(60)
+                if killed:
+                    logging.warning(f'Coin {self.name:5}    status:   Killed!')
